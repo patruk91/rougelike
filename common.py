f0e31542ae0708a43bc9e425e6c47bd0f0a53b3d
Krzysiek <krzysiekczaja@gmail.com>
2019-02-11 13:17:26 +0100

    Modify validate_string_input()
    Now contidion checking is optional

--- common.py
+++ common.py
@@ -1,7 +1,7 @@
 import ui
 
 
-def validate_string_input(string, condition):
+def validate_string_input(string, condition=""):
     """
     Validate if string from input is not empty and is in given condition.
     Condition must be in format: ["a", "b", "c".....]
@@ -12,16 +12,19 @@ def validate_string_input(string, condition):
     >>> validate_string_input("s", ["c"])
     ERROR: Input must be in ['c']
     False
-    >>> validate_string_input("content", "content")
+    >>> validate_string_input("Not_empty", "")
+    True
+    >>> validate_string_input("content", ["content"])
     True
     """
     is_string_correct = True
     if not string:
         ui.print_error("Input can't be empty")
         is_string_correct = False
-    if string:
-        if string not in condition:
-            ui.print_error("Input must be in {}".format(condition))
-            is_string_correct = False
+    if condition:
+        if string:
+            if string not in condition:
+                ui.print_error("Input must be in {}".format(condition))
+                is_string_correct = False
 
     return is_string_correct
