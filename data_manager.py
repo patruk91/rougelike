f8a37eda4b4f75b03f5a9cd7ae837e053d5e3fb0
Patryk Lichwala <lichwalapatryk@gmail.com>
2019-02-11 15:11:49 +0100

    Fix read maps from file into list of lists

--- data_manager.py
+++ data_manager.py
@@ -2,5 +2,6 @@ def get_maps_from_file(file_name):
     level_map = []
     with open(file_name, "r") as file_object:
         for line in file_object:
-            level_map.append(line.rstrip().split("\n"))
+            line = list(line.rstrip())
+            level_map.append(line)
     return level_map
