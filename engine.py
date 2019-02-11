2bf639fdcdf046ffbaf9a9c73a1d4ebb7a4f5b4d
Patryk Lichwala <lichwalapatryk@gmail.com>
2019-02-11 12:47:32 +0100

    Get user input in terminal console

---
+++ engine.py
@@ -0,0 +1,18 @@
+
+
+
+def get_char_in_terminal():
+    import sys
+    import tty
+    import termios
+    fd = sys.stdin.fileno()
+    old_settings = termios.tcgetattr(fd)
+    try:
+        tty.setraw(sys.stdin.fileno())
+        char = sys.stdin.read(1)
+    finally:
+        termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+    return char
+
+
+
