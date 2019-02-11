1baabad92ef7294d18ae1a912fde7d3db76bc783
Konrad Krzysztofiak <krzysztofiakk@gmail.com>
2019-02-11 14:56:50 +0100

    Add validate string input to menu module

--- rougelike.py
+++ rougelike.py
@@ -1,18 +1,20 @@
 import data_manager
 import character_creation
 import ui
+import common
 
 
 def menu():
     while True:
         answer = ui.menu_option()
-        if answer == "1":
-            pass
-        elif answer == "2":
-            pass
-        elif answer == "3":
-            pass
-        elif answer == "4":
-            exit()
+        if common.validate_string_input(answer, condition=["1", "2", "3", "4"]):
+            if answer == "1":
+                pass
+            elif answer == "2":
+                pass
+            elif answer == "3":
+                pass
+            elif answer == "4":
+                exit()
    
 menu()
