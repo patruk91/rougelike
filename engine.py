9fc00374fbd9ab016ad08794435ed9a8be9957e8
Patryk Lichwala <lichwalapatryk@gmail.com>
2019-02-11 12:48:50 +0100

    Move hero to the right side

--- engine.py
+++ engine.py
@@ -15,4 +15,30 @@ def get_char_in_terminal():
     return char
 
 
+def movement():
+    level = [["@", " ", " ", " ", " ", ], [" ", " ", " ", " ", " ", ]]  # + [[" ", " ", " ", " ", " ", ]] * 10
+    print(level)
+    while True:
+        # hero_indice = [column, row]
+        get_char = get_char_in_terminal()
+        if get_char == "d":
+            level = move_right(level)
+        if get_char == "q":
+            exit()
 
+
+def move_right(level):
+    hero_indice = [[line.index("@"), level.index(line)] for line in level if "@" in line][0]
+    hero_indice[0] += 1
+    for line in level:
+        for i in range(len(line)):
+            if line[i] == "@":
+                line[i] = " "
+                line[hero_indice[0]] = "@"
+            else:
+                line[i] = " "
+        print(level)
+        return level
+# x = get_char_in_terminal()
+# print(x)
+print(movement())
