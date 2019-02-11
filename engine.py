c9aa7e6ea6f212aa5426fca6bf7352ccbae44688
Patryk Lichwala <lichwalapatryk@gmail.com>
2019-02-11 14:12:17 +0100

    Allow hero move vertiacally

--- engine.py
+++ engine.py
@@ -16,7 +16,7 @@ def get_char_in_terminal():
 
 
 def movement():
-    level = [["@", " ", " ", " ", " ", ], [" ", " ", " ", " ", " ", ]]  # + [[" ", " ", " ", " ", " ", ]] * 10
+    level = [["@", " ", " ", " ", " ", ], [" ", " ", " ", " ", " ", ]] + [[" ", " ", " ", " ", " ", ]] * 3
     print(level)
     while True:
         # hero_indice = [column, row]
@@ -27,21 +27,40 @@ def movement():
         elif get_char == "a":
             MOVE_LEFT = -1
             level = move_horizontally(level, MOVE_LEFT)
+        elif get_char == "w":
+            MOVE_UP = -1
+            move_vertically(level, MOVE_UP)
+        elif get_char == "s":
+            MOVE_DOWN = 1
+            move_vertically(level, MOVE_DOWN)
+
         if get_char == "q":
             exit()
 
 
 def move_horizontally(level, move):
-    hero_indice = [[line.index("@"), level.index(line)] for line in level if "@" in line][0]
-    updated_hero_indice = hero_indice[0] + move
+    position = [[line.index("@"), level.index(line)] for line in level if "@" in line][0]
+    hero_horizontal_position = position[0] + move
     for horizontal in level:
         if "@" in horizontal:
             for indice in range(len(horizontal)):
                 horizontal[indice] = " "
-                horizontal[updated_hero_indice] = "@"
-
+                horizontal[hero_horizontal_position] = "@"
     print(level)
     return level
+
+
+def move_vertically(level, move):
+    position = [[line.index("@"), level.index(line)] for line in level if "@" in line][0]
+    level[position[1]][position[0]] = " "
+    position[1] += move
+    level[position[1]][position[0]] = "@"
+    print(level)
+
+
+
+
+
 # x = get_char_in_terminal()
 # print(x)
 print(movement())
