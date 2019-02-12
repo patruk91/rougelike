f8e5a47d94c98f24f04891e8f4ed8021dc10b86a
Konrad Krzysztofiak <krzysztofiakk@gmail.com>
2019-02-12 13:10:44 +0100

    Add items module

---
+++ items/items.py
@@ -0,0 +1,12 @@
+def weapons():
+    weapons_list = [("dagger", 2), ("axe", 3), ("pick", 3), ("stick", 1),
+                    ("infinity edge", 15), ("bloodrazor", 10)]
+    return weapons_list
+
+
+def food():
+    pass
+
+
+def clotches():
+    pass
