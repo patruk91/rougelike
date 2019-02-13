64f1ef852ac7a5e3de1455a721ea5c840f67f92f
Patryk Lichwala <lichwalapatryk@gmail.com>
2019-02-13 07:26:37 +0100

    Join two functions move( vertically, horizontally) into one

--- engine.py
+++ engine.py
@@ -4,7 +4,7 @@ import os
 import data_manager
 import ui
 import hot_cold
-import character_creation.character_creation as char_cr
+# import character_creation.character_creation as char_cr
 
 
 MOVE_ADD = 1
@@ -63,35 +63,28 @@ def handle_movement(filename, char_stats):
 
 
 def update_map(get_char, level_map, old_hero_coordinates, new_hero_coordinates):
-    if get_char in ["a", "d"]:
-        updated_level_map = move_horizontally(level_map, old_hero_coordinates, new_hero_coordinates)
-        return updated_level_map
-    elif get_char in ["s", "w"]:
-        updated_level_map = move_vertically(level_map, old_hero_coordinates, new_hero_coordinates)
+    """
+    Update map level or return old one, depend on user key pressed
+    :param get_char: char: key pressed by user
+    :param level_map: list of lists: designed map from text file
+    :param old_hero_coordinates: list: old(previous) hero coordinates on map
+    :param new_hero_coordinates: list: actual hero coordinates on map
+    :return: list of lists: updated level_map with placed hero at new coordinates
+             or old level_map with placed hero at old coordinates
+    """
+    if get_char in ["w", "a", "s", "d"]:
+        updated_level_map = hero_move(level_map, old_hero_coordinates, new_hero_coordinates)
         return updated_level_map
     return level_map
 
 
-def move_horizontally(level_map, old_hero_coordinates, new_hero_coordinates):
+def hero_move(level_map, old_hero_coordinates, new_hero_coordinates):
     """
     Move hero on map in horizontal directions
     :param level_map: list of lists: designed map from text file
     :param old_hero_coordinates: list: old(previous) hero coordinates on map
     :param new_hero_coordinates: list: actual hero coordinates on map
     :return: list of lists: updated level_map with placed hero at new coordinates
-    """
-    erase_hero = erase_old_hero_position(level_map, old_hero_coordinates)
-    updated_level_map = place_new_hero_position(erase_hero, new_hero_coordinates)
-    return updated_level_map
-
-
-def move_vertically(level_map, old_hero_coordinates, new_hero_coordinates):
-    """
-   Move hero on map in vertical directions
-   :param level_map: list of lists: designed map from text file
-   :param old_hero_coordinates: list: old(previous) hero coordinates on map
-   :param new_hero_coordinates: list: actual hero coordinates on map
-   :return: list of lists: updated level_map with placed hero at new coordinates
     """
     erase_hero = erase_old_hero_position(level_map, old_hero_coordinates)
     updated_level_map = place_new_hero_position(erase_hero, new_hero_coordinates)
@@ -124,16 +117,16 @@ def place_new_hero_position(level_map, hero_updated_position):
     return level_map
 
 
-def update_hero_coordinates(get_char, hero_position, move):
+def update_hero_coordinates(get_char, old_hero_coordinates, move):
     """
     Update hero coordinates/position
     :param get_char: char: key pressed by user
-    :param hero_position: list: hero position to update, depend on get_char
+    :param old_hero_coordinates: list: hero position to update, depend on get_char
     :param move: list: constant, hero can move forward or backward (depend on key pressed)
     :return: list: updated hero coordinates
     """
-    x_position = hero_position[0]
-    y_position = hero_position[1]
+    x_position = old_hero_coordinates[0]
+    y_position = old_hero_coordinates[1]
     move_add = move[0]
     move_sub = move[1]
 
@@ -178,7 +171,7 @@ def check_if_item_interaction(new_hero_coordinates, level_map):
 
 def trigger_interaction(new_hero_coordinates, level_map, char_stats):
     """
-    Trigger other functions due to a reaction with a given element
+    Trigger other functions due to a reaction with a given element on level map
     :param new_hero_coordinates: list: hero coordinates on map
     :param level_map: designed map from text file
     :param char_stats: dict: basic hero statistic
@@ -202,4 +195,4 @@ def trigger_interaction(new_hero_coordinates, level_map, char_stats):
 
 # x = get_char_in_terminal()
 # print(x)
-print(handle_movement(filename = 'levels/level1.txt', char_stats=char_cr.create_character(5)))
+# print(handle_movement(filename = 'levels/level1.txt', char_stats=char_cr.create_character(5)))
